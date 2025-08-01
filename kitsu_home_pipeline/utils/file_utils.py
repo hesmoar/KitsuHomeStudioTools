cf5570f1ce6c9587058d2df6caf7e4c709cdc6d2
hecberry <hecberry@gmail.com>
2025-08-01 08:54:43 -0700

    Moved some functions to more correct locations

--- kitsu_home_pipeline/utils/file_utils.py
+++ kitsu_home_pipeline/utils/file_utils.py
@@ -1,7 +1,7 @@
-   # In file_utils.py
 import os
 import tempfile
 import json
+import shutil
 from pathlib import Path
 
 def get_temp_dir(subfolder: str = None) -> Path:
@@ -25,3 +25,23 @@ def clean_up_temp_files():
     temp_dir = os.path.join(tempfile.gettempdir(), "KitsuTaskManager")
     if os.path.exists(temp_dir):
         shutil.rmtree(temp_dir)
+
+
+def get_unique_filename(base_name, directory, extension=""):
+    """Generate a unique filename with an incremental version number."""
+    directory = Path(directory)
+    if not directory.exists():
+        print(f"Error: Export directory '{directory}' does not exist.")
+        return None, None
+    extension = f".{extension}" if extension else ""
+    pattern = f"{base_name}_v*{extension}"
+    existing_versions = [ 
+        int(f.stem[len(base_name) + 2 :].split("_")[0])
+        for f in directory.glob(pattern)
+        if f.stem[len(base_name) + 2 :].split("_")[0].isdigit()
+    ]
+
+    version = max(existing_versions, default=0) + 1
+    filename = f"{base_name}_v{version:03d}{extension}"
+    full_file_path = directory / filename
+    return str(full_file_path), filename
